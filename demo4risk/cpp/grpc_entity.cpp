10112db9d906fd45fbb646905a52bef799f7ac41
mkdir <you@example.com>
2020-11-30 17:10:17 +0800

    riskcontroler中没有填is_snap

--- demo4risk/cpp/grpc_entity.cpp
+++ demo4risk/cpp/grpc_entity.cpp
@@ -3,6 +3,7 @@
 void quote_to_quote(const MarketStreamData* src, MarketStreamData* dst) {
     dst->set_exchange(src->exchange());
     dst->set_symbol(src->symbol());
+    dst->set_is_snap(src->is_snap());
 
     // 卖盘
     for( int i = 0 ; i < src->asks_size() ; ++i ) {
