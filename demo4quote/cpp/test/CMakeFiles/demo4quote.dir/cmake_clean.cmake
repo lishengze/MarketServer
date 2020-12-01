fc15aff8af1df3e48458158de3c35ab193037c4b
lishengze <lishengz.cool@hotmail.com>
2020-12-01 07:55:32 +0000

    fix frontserver cmake error

---
+++ demo4quote/cpp/test/CMakeFiles/demo4quote.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo4quote.dir/grpc_entity.cpp.o"
+  "CMakeFiles/demo4quote.dir/grpc_server.cpp.o"
+  "CMakeFiles/demo4quote.dir/main.cpp.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/account.grpc.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/account.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/gogo.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/kline_server.grpc.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/kline_server.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/quote_data.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/risk_controller.grpc.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/risk_controller.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/stream_engine.grpc.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/mnt/dev/bcts_quote/protos/cpp/stream_engine.pb.cc.o"
+  "CMakeFiles/demo4quote.dir/quote_mixer2.cpp.o"
+  "CMakeFiles/demo4quote.dir/quote_single.cpp.o"
+  "CMakeFiles/demo4quote.dir/redis_quote.cpp.o"
+  "CMakeFiles/demo4quote.dir/redis_quote_snap.cpp.o"
+  "CMakeFiles/demo4quote.dir/stream_engine.cpp.o"
+  "CMakeFiles/demo4quote.dir/stream_engine_config.cpp.o"
+  "demo4quote"
+  "demo4quote.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo4quote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
