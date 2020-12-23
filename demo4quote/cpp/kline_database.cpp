968471d9949e0cdd536cef2a76d20e7e3deb6a66
mkdir <yulingjie@wxblockchain.com>
2020-12-23 10:06:51 +0800

    。

--- demo4quote/cpp/kline_database.cpp
+++ demo4quote/cpp/kline_database.cpp
@@ -3,33 +3,23 @@
 
 string db_name = "klines.dat";
 
+// time_t转为时间索引
+// resolution=60：YYYYMMDD
+// resolution=3600: YYYYMM
 int timet_to_index(type_tick t, int resolution)
 {
     struct tm *newtime = localtime( (const long int*)&t );
-    if( resolution == 60 )
-        return newtime->tm_year * 10000 + newtime->tm_mon * 100 + newtime->tm_mday;
-    else if( resolution == 3600 )
+    if( resolution == 60 ) {
+        return t;
+        //return newtime->tm_year * 10000 + newtime->tm_mon * 100 + newtime->tm_mday;
+    } else if( resolution == 3600 ) {
         return newtime->tm_year * 100 + newtime->tm_mon;
-    else
+    } else {
         return 0;
-}
-
-void _mix_klines(vector<KlineData>& dst, const vector<KlineData>& src)
-{
-    map<type_tick, KlineData> tmp;
-    for( const auto& v : dst ) {
-        tmp[v.index] = v;
-    }
-    for( const auto& v : src ) {
-        tmp[v.index] = v;
-    }
-    dst.clear();
-    for( const auto& v : tmp ) {
-        dst.push_back(v.second);
     }
 }
 
-bool data_to_kline(const njson& data, KlineData& kline) {
+bool dbdata_to_kline(const njson& data, KlineData& kline) {
     kline.index = int(data[0].get<double>());
     kline.px_open.from(data[1].get<string>());
     kline.px_high.from(data[2].get<string>());
@@ -39,7 +29,7 @@ bool data_to_kline(const njson& data, KlineData& kline) {
     return true;
 }
 
-njson kline_to_data( const KlineData& kline) {
+njson kline_to_dbdata( const KlineData& kline) {
     njson ret;
     ret.push_back(kline.index);
     ret.push_back(kline.px_open.get_str_value());
@@ -50,17 +40,17 @@ njson kline_to_data( const KlineData& kline) {
     return ret;
 }
 
-string encode_klines(const vector<KlineData>& klines)
+string encode_dbdata(const vector<KlineData>& klines)
 {
     njson data;
     for( const auto& v : klines ) {
-        data.push_back(kline_to_data(v));
+        data.push_back(kline_to_dbdata(v));
     }
 
     return data.dump();
 }
 
-bool decode_json_klines( const string& data, vector<KlineData>& klines)
+bool decode_dbdata( const string& data, vector<KlineData>& klines)
 {    
     klines.clear();
 
@@ -77,7 +67,7 @@ bool decode_json_klines( const string& data, vector<KlineData>& klines)
 
     KlineData kline;
     for (auto iter = js.begin(); iter != js.end(); ++iter) {
-        data_to_kline(*iter, kline);
+        dbdata_to_kline(*iter, kline);
         klines.push_back(kline);
     }
     return true;
@@ -157,17 +147,17 @@ KlineDatabase::KlineDatabase()
 
 KlineDatabase::~KlineDatabase()
 {
-    if (loop_) {
-        if (loop_->joinable()) {
-            loop_->join();
+    if (_flush_thrd) {
+        if (_flush_thrd->joinable()) {
+            _flush_thrd->join();
         }
-        delete loop_;
+        delete _flush_thrd;
     }
 }
 
 void KlineDatabase::start()
 {
-    loop_ = new std::thread(&KlineDatabase::_loop, this);
+    _flush_thrd = new std::thread(&KlineDatabase::_flush_thread, this);
 
 }
 
@@ -222,18 +212,16 @@ bool KlineDatabase::get_kline(const TExchange& exchange, const TSymbol& symbol, 
     return _read_range_klines(exchange, symbol, resolution, begin_index, end_index, klines);
 }
 
-void KlineDatabase::_loop()
+void KlineDatabase::_flush_thread()
 {
     while( true ) 
-    {        
+    {
         unordered_map<TExchange, unordered_map<TSymbol, vector<KlineData>>> caches_min1;
         unordered_map<TExchange, unordered_map<TSymbol, vector<KlineData>>> caches_min60;
         {
             std::unique_lock<std::mutex> inner_lock{ mutex_caches_ };
-            caches_min1 = caches_min1_;
-            caches_min1_.clear();
-            caches_min60 = caches_min60_;
-            caches_min60_.clear();
+            caches_min1.swap(caches_min1_);
+            caches_min60.swap(caches_min60_);
         }
 
         // 写入db
@@ -245,78 +233,66 @@ void KlineDatabase::_loop()
     }
 }
 
-void KlineDatabase::_write_to_db_single(const TExchange& exchange, const TSymbol& symbol, int resolution, const vector<KlineData>& klines)
+void _mix_klines(vector<KlineData>& dst, const vector<KlineData>& src)
 {
-    int cur_index = 0;
-    int last_index = 0;
-    vector<KlineData> tmp;
-    for( size_t i = 0 ; i < klines.size() ; i ++ ) {
-
-        // 1分钟按日压缩存储，60分钟按月压缩存储
-        cur_index = timet_to_index(klines[i].index, resolution);
-        if( cur_index == 0 )
-            continue;
-
-
-        if( cur_index != last_index && last_index != 0) {
-            last_index = cur_index;
-            tmp.clear();
-            // 更新
-            vector<KlineData> last_klines;
-            _read_klines(exchange, symbol, resolution, last_index, last_klines);
-            _mix_klines(last_klines, tmp);
-            _write_klines(exchange, symbol, resolution, last_index, last_klines);
-        } else {
-            tmp.push_back(klines[i]);
-        }
+    map<type_tick, KlineData> tmp;
+    for( const auto& v : dst ) {
+        tmp[v.index] = v;
     }
-    
-    // 收尾
-    if( tmp.size() > 0 ) {
-        vector<KlineData> last_klines;
-        _read_klines(exchange, symbol, resolution, cur_index, last_klines);
-        _mix_klines(last_klines, tmp);
-        _write_klines(exchange, symbol, resolution, cur_index, last_klines);
+    for( const auto& v : src ) {
+        tmp[v.index] = v;
+    }
+    dst.clear();
+    for( const auto& v : tmp ) {
+        dst.push_back(v.second);
     }
 }
 
 void KlineDatabase::_write_to_db(const unordered_map<TExchange, unordered_map<TSymbol, vector<KlineData>>>& cache, int resolution)
 {
-    switch( resolution) {
-        case 60:
-        {
-            for( const auto& v: cache ) {
-                for( const auto& v2 : v.second ) {
-                    _write_to_db_single(v.first, v2.first, 60, v2.second);
+    for( const auto& exchange: cache ) {
+        for( const auto& symbol: v.second ) {
+            const vector<KlineData>& klines = v2.second;
+            
+            int cur_index = 0;
+            int last_index = 0;
+            vector<KlineData> tmp;
+
+            for( size_t i = 0 ; i < klines.size() ; i ++ ) {
+
+                // 计算索引值
+                cur_index = timet_to_index(klines[i].index, resolution);
+                if( cur_index == 0 )
+                    continue;
+
+
+                if( cur_index != last_index && last_index != 0) {
+                    last_index = cur_index;
+                    tmp.clear();
+                    // 更新
+                    vector<KlineData> last_klines;
+                    _read_klines(exchange, symbol, resolution, last_index, last_klines);
+                    _mix_klines(last_klines, tmp);
+                    _write_klines(exchange, symbol, resolution, last_index, last_klines);
+                } else {
+                    tmp.push_back(klines[i]);
                 }
             }
-            break;
-        }
-        case 3600:
-        {
-            for( const auto& v: cache ) {
-                for( const auto& v2 : v.second ) {
-                    _write_to_db_single(v.first, v2.first, 3600, v2.second);
-                }
+            
+            // 收尾
+            if( tmp.size() > 0 ) {
+                vector<KlineData> last_klines;
+                _read_klines(exchange, symbol, resolution, cur_index, last_klines);
+                _mix_klines(last_klines, tmp);
+                _write_klines(exchange, symbol, resolution, cur_index, last_klines);
             }
-            break;
-        }
-        default:
-        {
-            _log_and_print("unknown resolution %d", resolution);
-            return;
         }
     }
 }
 
-bool KlineDatabase::_init_db()
-{
-    return true;
-}
-
 bool KlineDatabase::_write_klines(const TExchange& exchange, const TSymbol& symbol, int resolution, int index, const vector<KlineData>& klines)
 {
-    string data = encode_klines(klines);
+    string data = encode_dbdata(klines);
 
     try
     {
@@ -352,7 +328,8 @@ bool KlineDatabase::_write_klines(const TExchange& exchange, const TSymbol& symb
                 stmtMin60ReplaceDataByExchangeSymbolIndex.exec();
                 break;
             }
-            default:{
+            default:
+            {
                 _log_and_print("%s-%s unknown resolution %d", exchange.c_str(), symbol.c_str(), resolution);
                 return false;
             }
@@ -390,7 +367,7 @@ bool KlineDatabase::_read_range_klines(const TExchange& exchange, const TSymbol&
                 while(stmtMin1SelectDataByExchangeSymbolIndexRange.executeStep()) {
                     int idx = stmtMin1SelectDataByExchangeSymbolIndexRange.getColumn("TimeIndex").getInt();
                     data = stmtMin1SelectDataByExchangeSymbolIndexRange.getColumn("Data").getString();
-                    decode_json_klines(data, _klines);
+                    decode_dbdata(data, _klines);
                     klines.insert(klines.begin(), _klines.begin(), _klines.end());
                     _log_and_print("get index=%d size=%lu", idx, _klines.size());
                 }
@@ -407,7 +384,7 @@ bool KlineDatabase::_read_range_klines(const TExchange& exchange, const TSymbol&
                 );
                 while(stmtMin60SelectDataByExchangeSymbolIndexRange.executeStep()) {
                     data = stmtMin60SelectDataByExchangeSymbolIndexRange.getColumn("Data").getString();
-                    decode_json_klines(data, _klines);
+                    decode_dbdata(data, _klines);
                     klines.insert(klines.begin(), _klines.begin(), _klines.end());
                 }
                 break;
@@ -478,5 +455,5 @@ bool KlineDatabase::_read_klines(const TExchange& exchange, const TSymbol& symbo
         return false;
     }
     
-    return decode_json_klines(data, klines);
+    return decode_dbdata(data, klines);
 }
