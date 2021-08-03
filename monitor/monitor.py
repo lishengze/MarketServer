06eb4f2e193dfa003f431e33660002b341f64299
lishengze <lishengz.cool@hotmail.com>
2021-08-03 10:52:46 +0800

    fix ding bug

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -92,7 +92,7 @@ class MonitorUtrade(object):
 
     def send_dingding_msg(self, msg, ding_type=DING_MODE_SOURCE):
         try:            
-            msg = self._server_name + ": " + msg
+            msg ="msg " + self._server_name + ": " + msg
 
             if ding_type == DING_MODE_RUN:
                 self._logger.Critical(msg)
