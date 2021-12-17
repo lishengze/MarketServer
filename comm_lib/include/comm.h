7bb70ad444d7e35d2051c4f2871fec89598a93b9
lishengze <lishengz.cool@hotmail.com>
2021-12-17 16:44:40 +0800

    add test_lib

---
+++ comm_lib/include/comm.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "base/cpp/basic.h"
+#include "base/cpp/base_data_stuct.h"
+
+#include "interface_define.h"
+
+#include "comm_declare.h"
+#include "comm_type_def.h"
+
+COMM_NAMESPACE_START
+
+class Comm
+{
+    public:
+        Comm(string server_address, 
+            QuoteSourceCallbackInterface* depth_engine,
+            QuoteSourceCallbackInterface* kline_engine,
+            QuoteSourceCallbackInterface* trade_engine,        
+            NET_TYPE net_type = NET_TYPE::KAFKA,
+            SERIALIZE_TYPE serialize_type = SERIALIZE_TYPE::JSON)
+;
+
+        Comm(string server_address,
+            NET_TYPE net_type = NET_TYPE::KAFKA,
+            SERIALIZE_TYPE serialize_type = SERIALIZE_TYPE::JSON,
+            QuoteSourceCallbackInterface* engine_center=nullptr);   
+
+        ~Comm();        
+
+        void launch();
+
+        void publish_depth(const SDepthQuote& quote);
+        void publish_kline(const KlineData& kline);
+        void publish_trade(const TradeData& trade);
+
+        void set_meta(const MetaType kline_meta, const MetaType depth_meta, const MetaType trade_meta);
+
+        void set_kline_meta(const MetaType meta);
+        void set_depth_meta(const MetaType meta);
+        void set_trade_meta(const MetaType meta);
+
+    private:
+        NetServer*          net_server_{nullptr};
+        Serializer*         serializer_{nullptr};
+};
+
+COMM_NAMESPACE_END
--- comm_lib/test_lib/test.cpp
+++ comm_lib/test_lib/test.cpp
@@ -4,6 +4,7 @@
 #include "../cpp/comm.h"
 #include "../cpp/interface_define.h"
 
+USING_COMM_NAMESPACE
 
 class TestEngine:public bcts::comm::QuoteSourceCallbackInterface
 {
@@ -23,14 +24,36 @@ class TestEngine:public bcts::comm::QuoteSourceCallbackInterface
     };
 };
 
-void test_lib()
+MetaType get_test_meta()
 {
-
+    MetaType test_meta;
+    std::set<string> exchange_set;
+    exchange_set.emplace("FTX");
+    test_meta["BTC_USDT"] = exchange_set;
+    return test_meta;
 }
 
 void test_code()
 {
+    TestEngine engine;
+    string server_address = "127.0.0.1:9117";
 
+    Comm comm(server_address, NET_TYPE::KAFKA, SERIALIZE_TYPE::JSON, &engine);
+
+    MetaType test_meta{std::move(get_test_meta())};
+    
+    comm.set_depth_meta(test_meta);
+
+    comm.set_kline_meta(test_meta);
+
+    comm.set_trade_meta(test_meta);
+
+    comm.launch();
+
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
 }
 
 void TestMain()
