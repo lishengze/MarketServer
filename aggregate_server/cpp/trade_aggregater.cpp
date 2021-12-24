fe59fd891820c3913b97b6363303d59ab0de0203
lishengze <lishengz.cool@hotmail.com>
2021-12-24 15:28:48 +0800

    change logs

--- aggregate_server/cpp/trade_aggregater.cpp
+++ aggregate_server/cpp/trade_aggregater.cpp
@@ -7,7 +7,7 @@ void TradeAggregater::on_trade(TradeData& ori_trade)
     {
         if (symbol_config_.find(ori_trade.symbol) == symbol_config_.end()) return;
 
-        if(ori_trade.symbol == "BTC_USDT") LOG_INFO(ori_trade.str());
+        // if(ori_trade.symbol == "BTC_USDT") LOG_INFO(ori_trade.str());
 
         // LOG->record_input_info(ori_trade.meta_str(), ori_trade);
 
