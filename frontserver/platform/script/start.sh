552654805332c91a30035058cd8bac995c080471
lishengze <lishengz.cool@hotmail.com>
2022-02-22 15:46:54 +0800

    update front_server

--- frontserver/platform/script/start.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-nohup /mnt/bcts_quote/frontserver/platform/build/front_server /mnt/bcts_quote/frontserver/platform/build/config.json  > /mnt/bcts_quote/frontserver/platform/build/front_server.log &
-
-sleep 1s
-
-ps -aux|grep front_server
--- frontserver/platform/script/stop.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-
-PID=`ps aux|grep front_server |grep -v grep | awk '{print $2}'` 
-kill -9 $PID
-
-sleep 2s
-
-ps -aux|grep front_server
